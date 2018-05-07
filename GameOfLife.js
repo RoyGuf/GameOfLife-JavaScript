d34eed91fbbbe5ed007327be8f6a0e90cfe97de9
Roy Mizrahi <34283540+RoyGuf@users.noreply.github.com>
2018-05-07 22:42:34 +0300

    Add 'free drawing' feature

--- GameOfLife.js
+++ GameOfLife.js
@@ -1,14 +1,14 @@
 var rows = 200;
+var a = [];
+var b = [];
 var Grid = buildGrid(rows);
 var miGrid = Grid;
 
-Grid[1][1] = Math.floor(1.5);
-Grid[3][1] = Math.floor(1.5);
-Grid[4][1] = Math.floor(1.5);
-Grid[2][1] = Math.floor(1.5);
 
-//fillGrid();
-randomGrid();
+
+addEvent();
+///fillGrid2();
+///randomGrid();
 
 Start();
 
@@ -32,7 +32,42 @@ function randomGrid(){
             Grid[j][k] = Math.floor(Math.random() * 2);
         }
     }
+    drawGame();
 }
+
+function addEvent(){
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+    ctx.clearRect(0, 0, 300, 300); 
+    ctx.fillStyle = "#264a78";
+    ctx.strokeStyle = "#222222";
+    var drawing = false;
+    var mousePos = { x:0, y:0 };
+    var lastPos = mousePos;
+    canvas.addEventListener("mousedown", function (e) {
+            drawing = true;
+      lastPos = getMousePos(canvas, e);
+    }, false);
+    canvas.addEventListener("mouseup", function (e) {
+      drawing = false;
+      fillGrid2();
+    }, false);
+    canvas.addEventListener("mousemove", function (e) {
+      mousePos = getMousePos(canvas, e);
+    }, false);
+
+    function getMousePos(canvasDom, mouseEvent) {
+        var rect = canvasDom.getBoundingClientRect();
+        console.log(Grid[0][0]);
+        console.log(a[0]);
+        a.push(mousePos);
+        return {
+            x: mouseEvent.clientX - rect.left,
+            y: mouseEvent.clientY - rect.top
+          };
+        }
+     
+}   
 
 function fillGrid(){ ///fill fields with 0 if they are not 1
     for(var j=0; j<rows; j++){
@@ -44,10 +79,22 @@ function fillGrid(){ ///fill fields with 0 if they are not 1
     }
 }
 
+function fillGrid2(){ 
+    for(var j=0; j<a.length; j++){
+        var x = Math.round(a[j].x);
+        var y = Math.round(a[j].y);
+        Grid[y][x] = Math.floor(1.5);
+        ///console.log(Grid[y][x])
+        fillGrid();
+        drawGame();
+
+    }
+}
+
 function drawGame(){
     var c = document.getElementById("myCanvas");
     var ctx = c.getContext("2d");
-    ctx.clearRect(0, 0, 300, 300); 
+    ctx.clearRect(0, 0, 200, 200); 
     ctx.fillStyle = "#264a78";
     for(var j=1; j<rows; j++){
         for(var k=1; k<rows; k++){
@@ -109,9 +156,27 @@ function updateGame(){ ///game logic
     }
 }
 
-///button controll
+///buttons controll
 var myButton = document.getElementById('btn');
 var myInterval = null;
+var ran = document.getElementById('random');
+var clear = document.getElementById('clear');
+
+clear.addEventListener('click', function(){
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+    ctx.clearRect(0, 0, 200, 200); 
+    a = [];
+    for(var j=0; j<rows; j++){
+        for(var k=0; k<rows; k++){
+            Grid[j][k] = 0;
+        }
+    }
+})
+
+ran.addEventListener('click', function(){
+    randomGrid();
+})
 
 myButton.addEventListener("click", function(){
   if(myInterval){ 
